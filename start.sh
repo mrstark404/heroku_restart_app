d108da31d53aee5c9878473e64c9c840723d8796
mrstark404 <112131793+mrstark404@users.noreply.github.com>
2025-02-16 11:44:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Update and install required dependencies
-echo "Updating system and installing dependencies..."
-apt update && apt upgrade -y
-apt install curl wget software-properties-common -y
-
 # Install Heroku CLI
 echo "Installing Heroku CLI..."
 curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
